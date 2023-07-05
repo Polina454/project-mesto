2ca818a4d3ae775d38e7507d7b7b8fb6b1d9ef5e
Polina Blizniuk <polinahvoryh@yandex.ru>
2023-07-05 15:17:06 +0300

    test

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,4 +7,4 @@ export const profileTitleInput = document.querySelector('.popup__input_title');
 export const editButton = document.querySelector('.profile__edit-button');
 export const profileAvatarButton = document.querySelector('.profile__icon');
 export const addButton = document.querySelector('.profile__add-button')
-/////////////////////
+
